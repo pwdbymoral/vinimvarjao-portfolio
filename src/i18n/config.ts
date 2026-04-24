45b33d8774a662bc61775a0a2eec4e723222ad05
Vinícius Varjão <moraesvxp@gmail.com>
2026-04-24 17:14:43 -0300

    feat: implement i18n with en/pt locales

---
+++ src/i18n/config.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+import enTranslations from "./locales/en.json";
+import ptTranslations from "./locales/pt.json";
+
+i18n
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		resources: {
+			en: { translation: enTranslations },
+			pt: { translation: ptTranslations },
+		},
+		fallbackLng: "en",
+		interpolation: {
+			escapeValue: false,
+		},
+		detection: {
+			order: ["path", "localStorage", "navigator"],
+			lookupFromPathIndex: 0,
+			caches: ["localStorage"],
+		},
+	});
+
+export default i18n;
